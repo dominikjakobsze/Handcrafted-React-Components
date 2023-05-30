71dca02f2919ddbf15a7297d61d9da9b79c40347
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-05-30 22:30:12 +0200

    gsap added

--- src/MainContainer.jsx
+++ src/MainContainer.jsx
@@ -5,6 +5,11 @@ import PoweredByCarousel from "./components/PoweredByCarousel";
 import Toggler from "./ReadyToUse/Toggler/Toggler";
 
 const MainContainer = () => {
+
+    const onToggle = (toggler) => {
+        console.log("current state of toggler: ",toggler)
+    };
+
     return (<>
         <div 
             className="p-5 gap-5 w-full flex flex-row flex-wrap justify-center items-start bg-[#000000]"
@@ -14,10 +19,10 @@ const MainContainer = () => {
             <Text width="48%" padding={"1.25rem"}>Developed with the goal of enhancing developer productivity and providing consistent user experiences, handcrafted React components offer a range of benefits. By encapsulating specific functionality and styling into self-contained modules, these components enable developers to build complex user interfaces more efficiently.</Text>
             <Header text="Powered by GSAP, React Icons, Tailwind CSS, Embla Carousel" fontSize={"1.75rem"} textAlign={"left"}/>
             <PoweredByCarousel/>
-            <Header text="Read To Use" marginTop={"1rem"} marginBottom={"1rem"}/>
+            <Header text="Ready To Use" marginTop={"1rem"} marginBottom={"1rem"}/>
             <Header text="Toggler" width={"50%"} fontSize={"1.75rem"} textAlign={"left"} fontWeight={"normal"}/>
             <Text bg={false} color={"#007aff"}>When building a web application, toggling an element is one of the key features you are likely to come across and may need to implement in your project.</Text>
-            <Toggler onToggle={(toggler) => {console.log("current state of toggler: ",toggler)}}/>
+            <Toggler onToggle={(toggler) => onToggle(toggler)}/>
         </div>
     </>);
 };
--- src/ReadyToUse/Toggler/Toggler.jsx
+++ src/ReadyToUse/Toggler/Toggler.jsx
@@ -1,19 +1,56 @@
 import React from "react";
+import { gsap } from "gsap";
 
 const Toggler = ({onToggle}) => {
-
     const [toggler, setToggler] = React.useState(false);
+    const togglerContainerRef = React.useRef(null);
+    const togglerButtonRef = React.useRef(null);
 
-    React.useEffect(() => {
+    const onToggleAnimation = () => {
+        return new Promise((resolve) => {
+            const togglerTimeline = gsap.timeline({
+                onComplete: () => {
+                    console.log("animation completed");
+                    resolve();
+                },
+                paused: true,
+            });
+            togglerTimeline.to(togglerButtonRef.current, {
+                duration: 0.25,
+                ease: "back",
+                x: toggler ? "125%" : "0%",
+            });
+            togglerTimeline.to(togglerContainerRef.current, {
+                duration: 0.25,
+                ease: "power4",
+                backgroundColor: toggler ? "#007aff" : "#39393d",
+            }, "-=0.20");
+            console.log(togglerTimeline.duration());
+            console.log(togglerTimeline.isActive());
+            console.log(togglerTimeline.isActive());
+            
+        });
+    };
+
+    const onToggleInside = async (toggler) => {
+        await onToggleAnimation()
         onToggle(toggler);
+    };
+    React.useEffect(() => {
+        onToggleInside(toggler);
     }, [toggler]);
 
     return (<>
-        <div className="w-[75px] h-[30px] rounded-full bg-[#39393d] relative">
-            <div className="rounded-full cursor-pointer
-            w-[35px] h-[35px] bg-white absolute top-[50%]
-            left-[20%] translate-x-[-50%] translate-y-[-50%]"
+        <div 
+            className="w-[75px] h-[30px] rounded-full bg-[#39393d] relative cursor-pointer"
+            ref={togglerContainerRef}
             onClick={() => setToggler((prev) => !prev)}
+        >
+            <div 
+                className="rounded-full
+                w-[35px] h-[35px] bg-white absolute top-[50%]
+                left-[20%] translate-x-[-50%] translate-y-[-50%]"
+                ref={togglerButtonRef}
             ></div>
         </div>
     </>);
